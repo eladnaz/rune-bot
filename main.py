634cca31ceb578ebab72b59d4654e960653c2967
eladnaz <shuen5035@gmail.com>
2021-08-10 10:26:41 +0800

    added images to branch

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello_world")
+import pyautogui as gui
+import win32gui
+MAPLE_X1 = 0
+MAPLE_X2 = 0
+MAPLE_Y1 = 0
+MAPLE_Y2 = 0
+MAPLE_NAME = "Swordie"
+
+def getMapleSize():
+    maple_win = win32gui.FindWindow(None,MAPLE_NAME)
+    MAPLE_X1,MAPLE_Y1,MAPLE_X2,MAPLE_Y2 = win32gui.GetWindowRect(maple_win)
+    print(MAPLE_X1)
+    print(MAPLE_X2)
+    print(MAPLE_Y1)
+    print(MAPLE_Y2)
+
+getMapleSize()
